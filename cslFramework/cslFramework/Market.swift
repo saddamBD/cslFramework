5dc23ff03c0604e25e43f7c08fbddd11ccdc9b84
saddam <saddam.ruet@gmail.com>
2018-09-13 18:18:11 +0600

    market file is added

---
+++ cslFramework/cslFramework/Market.swift
@@ -0,0 +1,21 @@
+//
+//  Market.swift
+//  cslFramework
+//
+//  Created by Saddam Hossain on 13/9/18.
+//  Copyright © 2018 Saddam Hossain. All rights reserved.
+//
+
+import Foundation
+
+
+public class Market {
+    
+    private init(){}
+    
+    public static func marktCap()-> String
+    {
+    return "Now Apple market value is 1 Trillion!!!!."
+    }
+    
+}
