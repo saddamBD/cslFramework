d9e22bbf10da0813ee36581f61b0ff060ce90608
saddam <saddam.ruet@gmail.com>
2018-09-13 15:40:35 +0600

    basic function is added

---
+++ cslFramework/cslFramework/Apple.swift
@@ -0,0 +1,23 @@
+//
+//  Apple.swift
+//  cslFramework
+//
+//  Created by Saddam Hossain on 13/9/18.
+//  Copyright © 2018 Saddam Hossain. All rights reserved.
+//
+
+import Foundation
+
+
+public class Apple {
+    
+    
+    private init(){}
+    
+    public static func applePark()-> String{
+        
+        return "Now ios 12.0 is availabe."
+        
+    }
+    
+}
---
+++ cslFramework/cslFramework/cslFramework.h
@@ -0,0 +1,19 @@
+//
+//  cslFramework.h
+//  cslFramework
+//
+//  Created by Saddam Hossain on 13/9/18.
+//  Copyright © 2018 Saddam Hossain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for cslFramework.
+FOUNDATION_EXPORT double cslFrameworkVersionNumber;
+
+//! Project version string for cslFramework.
+FOUNDATION_EXPORT const unsigned char cslFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cslFramework/PublicHeader.h>
+
+
